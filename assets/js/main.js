84aead46f814ae45b4d807db47d2311cc783f26d
Abderrahim ZEGHICHI <50805730+abzechix@users.noreply.github.com>
2021-09-27 17:08:15 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$(function () {
+  new WOW().init();
+
+  "use strict"
+ $(window).on('scroll', function (event) {
+        var scroll = $(window).scrollTop();
+        if (scroll < 100) {
+            $(".nav-area").removeClass("sticky");
+        } else {
+            $(".nav-area").addClass("sticky")
+        }
+    });
+  // tiny slider
+  var slider = tns({
+    container: ".about-active",
+    items: 1,
+    slideBy: "page",
+    autoplay: false,
+    nav: false,
+    controlsText: [
+      '<i class="slideCtrl fa fa-angle-left prev" aria-hidden="true"></i>',
+      '<i class=" slideCtrl fa fa-angle-right next" aria-hidden="true"></i>',
+    ],
+  });
+  $.scrollIt();
+});
